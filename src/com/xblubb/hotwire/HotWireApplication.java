6b9b66044670dba8ffe494f9c97e8401e26e641b
Exeu <jan@pixel-web.org>
2010-08-18 00:16:06 +0200

    Project Setup

---
+++ src/com/xblubb/hotwire/HotWireApplication.java
@@ -0,0 +1,18 @@
+package com.xblubb.hotwire;
+
+import android.app.Application;
+
+public class HotWireApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		// do sth.
+	}
+
+
+	@Override
+	public void onTerminate()
+	{
+		// do sth.
+	}
+}
